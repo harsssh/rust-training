ab87f524a48379a887c82d2da417d98c684a462a
harsh <66548698+harsssh@users.noreply.github.com>
2024-04-04 07:13:36 +0900

    excercise

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn greet_map(id: i32, name: &str) -> std::collections::HashMap<i32, String> {
+    let mut map = std::collections::HashMap::new();
+    let value = format!("Hello, {name}");
+    map.insert(id, value);
+    map
+}
+
 fn main() {
-    println!("Hello, world!");
+    let map = greet_map(1, "Alice");
+    println!("{:?}", map)
 }
